d08aa545201022bae76bfae15faa49264bcbd212
bderevyaga <bogdan.derevyaga@gmail.com>
2021-01-31 21:49:47 +0200

    сode cleaning

--- gulpfile.js
+++ gulpfile.js
@@ -5,16 +5,22 @@ const imagemin = require('gulp-imagemin');
 const spritesmith = require('gulp.spritesmith');
 
 gulp.task('sprite', () => {
-  gulp.src('./icon/*.png').pipe(spritesmith({
-    imgName: 'sprite.png',
-    cssName: 'sprite.css',
-    cssFormat: 'css'
-  }))
+  gulp.src('./icon/*.png')
+    .pipe(spritesmith({
+      imgName: 'sprite.png',
+      cssName: 'sprite.css',
+      cssFormat: 'css'
+    }))
     .pipe(gulp.dest('./dist'));
 });
 
-gulp.task('img', () => {
+gulp.task('sprite-min', () => {
   gulp.src('./dist/*.png')
     .pipe(imagemin())
     .pipe(gulp.dest('./dist'))
 });
+
+gulp.task('sprite-all', [
+  'sprite',
+  'sprite-min'
+]);
